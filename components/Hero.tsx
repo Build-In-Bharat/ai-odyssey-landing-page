37f0b7f260931a3b184098ea80ce768a1e0bbd23
aakash <mahajanaakash108@gmail.com>
2024-11-07 18:24:27 +0530

    Changes

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -14,7 +14,7 @@ const Hero = () => {
         <div className='w-full md:w-[60vw] lg:w-[48vw] text-white text-4xl md:text-5xl lg:text-6xl leading-[45px] md:leading-[60px] lg:leading-[70px] italic'>Unlock the Future of AI 
         with Microsoft AI Odyssey</div>
         <div className='w-full md:w-[40vw] lg:w-[30vw] my-4 md:my-5 text-[#C6C6C6] text-lg md:text-xl'>
-        Step into the future of AI with Microsoft Azure's certification course.
+        Step into the future of AI with Microsoft Azure&apos;s certification course.
         </div>
         <div>
           <button className='bg-white text-black px-8 md:px-10 lg:px-12 py-[12px] md:py-[14px] rounded-full font-semibold text-sm md:text-base'>Sign up</button>
